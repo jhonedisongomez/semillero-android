4fb65e59935cc29c075851bfd9f0a70ef04f2ae1
jhonedisongomez <jhon_edinson11@hotmail.com>
2017-11-09 16:35:00 -0500

    initial commit

---
+++ app/src/main/java/com/coapps/semillero/presentation/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.coapps.semillero.presentation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.coapps.semillero.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
